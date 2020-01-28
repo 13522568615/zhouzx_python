97b8d071f88dc5a9ad5c7e864938da626b8231f6
周宗鑫 <zhouzongxin_94@163.com>
2020-01-28 09:14:05 +0800

    20200128

--- pythonBase/1-100的素数.py
+++ pythonBase/1-100的素数.py
@@ -1,12 +1,19 @@
 #打印1-100的素数
 sum = 0
+count = 0
 for i in range (1, 100):
+    suShu = True#suShu等于真
     for j in range(2, i):
         if i%j == 0:
+            suShu = False
             break
-    else:
-        print(i)
+    #每输出5次换行
+    if suShu:#如果suShu是真的话
+        print(i,end=" ")#输出i，并且空格
+        count += 1#count自加1
+        if (count%5 == 0):#如果count等于5时，5%5==0时
+            print("\n")#换行
 
     #求素数的和
     sum += i
-print(sum)
+print(sum, end="\n")
