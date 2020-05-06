11fd82ea772c44de1bdf34dc4226754316a87b5c
zhouzongxin_wb <zhouzongxin_94@163.com>
2020-05-06 10:29:47 +0800

    20200506-test

--- pythonBase/类.py
+++ pythonBase/类.py
@@ -4,7 +4,19 @@ class MyClass:
     def hello(self):
         return 'hello'
 
+    a = 1
+    if a < 0:
+        print("负数")
+    else:
+        if a == 0:
+            print("0")
+        else:
+            print("正数")
+
 x = MyClass()
 
+print(x.a)
 print(x.i)
 print(x.hello())
+
+
